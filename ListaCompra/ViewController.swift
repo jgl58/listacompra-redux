8399fb2809c68cc877089993def109d9c15891bb
Otto Colomina <otto@dccia.ua.es>
2020-01-30 14:30:07 +0100

    Initial Commit

---
+++ ListaCompra/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ListaCompra
+//
+//  Created by Otto Colomina Pardo on 30/01/2020.
+//  Copyright © 2020 Otto Colomina Pardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
